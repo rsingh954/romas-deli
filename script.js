1e2960cccf061dc5459fc284827433223df08c64
rajmahal4 <rsingh6@mail.sfsu.edu>
2022-04-24 11:05:59 -0700

    almost done

--- script.js
+++ script.js
@@ -20,17 +20,21 @@ function loadMenu(data) {
     const description = document.createElement("div");
     console.log(sandwiches);
     card.classList.add("card");
+
     title.classList.add("card-title");
 
     priceContainer.classList.add("price");
     textContainer.classList.add("left");
 
     description.classList.add("description");
-
+    console.log(sandwiches);
     title.textContent = prop;
-    description.textContent = !sandwiches[prop].description
-      ? "May include Mayonnaise, mustard, garlic sauce, lettuce, tomato, onions, and pickles."
-      : sandwiches[prop].description;
+    description.textContent =
+      prop == "Gyro"
+        ? ""
+        : !sandwiches[prop].description
+        ? "May include Mayonnaise, mustard, garlic sauce, lettuce, tomato, onions, and pickles."
+        : sandwiches[prop].description;
     priceSmall.textContent = "S " + sandwiches[prop].small + " ";
     priceLarge.textContent =
       sandwiches[prop].large === undefined ? "" : "L " + sandwiches[prop].large;
@@ -47,3 +51,28 @@ function loadMenu(data) {
     special.appendChild(card);
   }
 }
+let observedElements = document.querySelectorAll(".about-container");
+
+const options = {
+  //define your options
+  threshold: 0.5,
+};
+
+const inViewCallback = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // define the event/property you want to use
+      //do something with the element, in our case, add a class
+      entry.target.classList.add("inview");
+    } else {
+      // OPTIONAL, in case you want to do something once the intersection is done
+    }
+  });
+};
+
+// create a new instance using our callback which contains our elements and actions, using the options we defined
+let observer = new IntersectionObserver(inViewCallback, options);
+
+observedElements.forEach((element) => {
+  observer.observe(element); // run the observer
+});
